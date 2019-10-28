f73ad32e9413bfea7b3a4d4605c65e516b67e8e4
gittergit <57091190+gittergit@users.noreply.github.com>
2019-10-28 02:25:43 -0700

    Update env_var.js

--- env_var.js
+++ env_var.js
@@ -2,7 +2,7 @@ const {exec}= require('child_process');
 console.log(process.env.foo);
 process.env.foo='fox';
 console.log(process.env.foo);
-exec(`export foo=${process.env.foo}`,(err, stdout, stderr)=>{
+exec(`set foo=${process.env.foo}`,(err, stdout, stderr)=>{
   if(err){
     console.log(err)
   }
